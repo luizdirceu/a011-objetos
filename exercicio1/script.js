be0ef947832a5118b93f11a955cf6a0c360344c4
Luiz Dirceu <juninholdmarques@gmail.com>
2022-08-02 20:44:37 -0300

    atividade

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arraySacolao = []
+const fruta1 = {nome: "maçã", preço: 5, disponibilidade: true} 
+const fruta2 = { nome: "banana", preço: 12, disponibilidade: true }
+const fruta3 = {nome: "laranja", preço: 9, disponibilidade: false}
+
+arraySacolao.push(fruta1, fruta2, fruta3)
+console.log(arraySacolao)
